368b7611e5ffd883606b54bf980ace6d28203975
eveleh27 <eveleh27@bergen.org>
2026-07-08 15:59:10 -0500

    Added templates file

---
+++ constants.py
@@ -0,0 +1,29 @@
+SPECTRAL_LINES = {
+    'Hα': 6562.8,
+    'Hβ': 4861.3,
+    'Hγ': 4340.5,
+    'Hδ': 4101.7,
+    'Na I D': 5892.9,
+    'Mg I b': 5176.7,
+    'Ca II K': 3933.7,
+    'Ca II H': 3968.5,
+    'G band': 4304.0,
+    'He I': 5875.6,
+    'He II': 4685.7
+}
+
+template_files = [
+    'spDR2-000.fit', 'spDR2-001.fit', 'spDR2-002.fit', 'spDR2-003.fit', 
+    'spDR2-004.fit', 'spDR2-005.fit', 'spDR2-006.fit', 'spDR2-007.fit', 
+    'spDR2-008.fit', 'spDR2-009.fit', 'spDR2-010.fit', 'spDR2-011.fit', 
+    'spDR2-012.fit', 'spDR2-013.fit', 'spDR2-014.fit', 'spDR2-015.fit', 
+    'spDR2-016.fit', 'spDR2-017.fit',  'spDR2-018.fit', 'spDR2-019.fit', 
+    'spDR2-020.fit', 'spDR2-021.fit', 'spDR2-022.fit'
+]
+
+template_names = [
+    'O', 'OB_transition', 'B', 'A', 'A', 'FA_transition', 'F', 
+    'F', 'G', 'G', 'K', 'M1', 'M3', 'M5', 'M8', 'L1', 'MWD', 
+    'Carbon', 'Carbon', 'Carbon', 'White_Dwarf', 'B_White_Dwarf', 
+    'Low_metallicityu_K_subdwarf'
+]
