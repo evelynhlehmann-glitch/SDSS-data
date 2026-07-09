d4fc488a43d7bb155825fbb280afe151467d76b8
eveleh27 <eveleh27@bergen.org>
2026-07-08 20:50:39 -0500

    fixed subplot formatting

--- main.py
+++ main.py
@@ -3,41 +3,29 @@ from classification import classify_star
 from spectrum import startype
 from templates import *
 
-plotspectrum(2934, 518)
-startype(2934, 518)
+def run_demo():
+    cases = [
+        (2934, 518),
+        (2260, 143)
+    ]
+    for plate, fiberID in cases:
+        plotspectrum(plate, fiberID)
+        plot_spectrum_and_template(plate, fiberID, templates['B'])
 
-plot_spectrum_and_template(2934, 518, templates['B'])
+    test_cases = [
+        (3407, 367),
+        (2260, 143),
+        (3311, 27),
+        (3311, 22),
+        (1150, 221),
+        (542, 514),
+        (283, 120)
+    ]
 
-plotspectrum(2260, 143)
-startype(2260, 143)
+    for plate, fiberID in test_cases:
+        classify_star(plate, fiberID, templates)
+        startype(plate, fiberID)
 
-
-
-classify_star(plate=3407, fiberID=367, templates_dict=templates)
-startype(plate=3407, fiberID=367)
-
-classify_star(plate=2260, fiberID=143, templates_dict=templates)
-startype(plate=2260, fiberID=143)
-
-classify_star(plate=2934, fiberID=518, templates_dict=templates)
-startype(plate=2934, fiberID=518)
-
-classify_star(plate=3856, fiberID=182, templates_dict=templates)
-startype(3856, 182)
-
-classify_star(plate=3311, fiberID=27, templates_dict=templates)
-startype(3311, 27)
-
-classify_star(plate=3311, fiberID=22, templates_dict=templates)
-startype(3311, 22)
-
-classify_star(plate=1150, fiberID=221, templates_dict=templates)
-startype(1150, 221)
-
-classify_star(plate=542, fiberID=514, templates_dict=templates)
-startype(542, 514)
-
-classify_star(plate=283, fiberID=120, templates_dict=templates)
-startype(283, 120)
-
-plt.show()
+if __name__ == "__main__":
+    run_demo()
+    plt.show()
--- plotting.py
+++ plotting.py
@@ -17,33 +17,38 @@ def plot_spectral_lines(ax, lines_to_plot):
         trans = ax.get_xaxis_transform()
         ax.text(wavelength, 1.02, name, rotation =90, va= 'bottom', ha= 'center', fontsize = 6, color = color, alpha = 0.7, transform = trans)
 
+def legend(fig, lines_to_plot):
+    colors = plt.cm.tab10.colors
     legend_lines = [Line2D([0], [0], color=colors[i % len(colors)], lw=1.5, linestyle=':') for i in range(len(lines_to_plot))]
-    ax.legend(legend_lines, list(lines_to_plot.keys()), title="Spectral Lines", loc='upper right')
+    fig.legend(legend_lines, list(lines_to_plot.keys()), title="Spectral Lines", loc=(.83, .57))
+    fig.legend(legend_lines, list(lines_to_plot.keys()), title="Spectral Lines", loc=(.83, .12))
 
 def plotspectrum(plate, fiberID):
     sp = SDSS.get_spectra(plate=plate, fiberID=fiberID)[0]
     wavelength, flux = get_spectrum_data(sp)
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(9, 8))
+    ax1.plot(wavelength, flux, color='blue', lw=0.5)   
+    fig.subplots_adjust(hspace=0.3, right = 0.8, top = 0.9)
 
-    plt.figure(figsize=(11,7))
-    plt.plot(wavelength, flux, color='blue', lw=0.5)
-    plot_spectral_lines(plt.gca(), {k: v for k, v in SPECTRAL_LINES.items() if wavelength.min() < v < wavelength.max()})
-    
-    plt.xlabel('Wavelength (Å)')
-    plt.ylabel('Flux')
-    plt.title(f'SDSS Spectrum - Plate {plate}, Fiber {fiberID}')
-    plt.grid(alpha=0.3)
+
+    ax1.set_xlabel('Wavelength (Å)')
+    ax1.set_ylabel('Flux')
+    ax1.set_title(f'SDSS Spectrum - Plate {plate}, Fiber {fiberID}')
+    ax1.grid(alpha=0.3)
 
     # plt.show()
-    
-    plt.figure(figsize=(11,7))
-    plot_spectral_lines(plt.gca(), {k: v for k, v in SPECTRAL_LINES.items() if wavelength.min() < v < wavelength.max()})
-
-    plt.xlabel('Wavelength (Å)')
-    plt.ylabel('Flux')
-    plt.title(f'SDSS Normalized Spectrum - Plate {plate}, Fiber {fiberID}')
-    plt.grid(alpha=0.3)
+    ax2.set_xlabel('Wavelength (Å)')
+    ax2.set_ylabel('Flux')
+    ax2.set_title(f'SDSS Normalized Spectrum - Plate {plate}, Fiber {fiberID}')
+    ax2.grid(alpha=0.3)
     norm_flux = normalize(flux, wavelength)
-    plt.plot(wavelength, norm_flux, color='indigo', lw=0.5)
+    ax2.plot(wavelength, norm_flux, color='indigo', lw=0.5)
+    lines_to_plot = {k: v for k, v in SPECTRAL_LINES.items() if wavelength.min() < v < wavelength.max()}
+    plot_spectral_lines(ax1, lines_to_plot)
+    plot_spectral_lines(ax2, lines_to_plot)
+    legend(fig, lines_to_plot)
+    fig.subplots_adjust(hspace=0.3, right = 0.8, top = 0.9)
+
     # plt.show()
 
 def plot_spectrum_and_template(plate, fiberID, template):
