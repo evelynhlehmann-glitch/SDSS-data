557268a9432215359c9434cc16146f8f2ab05947
eveleh27 <eveleh27@bergen.org>
2026-07-22 16:26:33 -0500

    set up uv

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from templates import *
 from coordsearch import *
 
 def run_demo():
-    # cases = [
-    #     (2934, 518),
-    #     (2260, 143)
-    # ]
-    # for plate, fiberID in cases:
-    #     plotspectrum(plate, fiberID)
-    #     plot_spectrum_and_template(plate, fiberID, templates['B'])
+    cases = [
+        (2934, 518),
+        (2260, 143)
+    ]
+    for plate, fiberID in cases:
+        plotspectrum(plate, fiberID)
+        plot_spectrum_and_template(plate, fiberID, templates['B'])
 
     # test_cases = [
     #     (3407, 367),
